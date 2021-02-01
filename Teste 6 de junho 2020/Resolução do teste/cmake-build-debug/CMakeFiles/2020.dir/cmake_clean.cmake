fb7fc0676a96ae3b348716ba576ed3040b29a6b7
Francisco Reis Izquierdo A93241 <62063067+FranciscoReisIzquierdo@users.noreply.github.com>
2021-02-01 18:34:03 +0000

    Enunciado/Resolução do teste

---
+++ Teste 6 de junho 2020/Resolução do teste/cmake-build-debug/CMakeFiles/2020.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2020.exe"
+  "2020.exe.manifest"
+  "2020.pdb"
+  "CMakeFiles/2020.dir/main.c.obj"
+  "lib2020.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
